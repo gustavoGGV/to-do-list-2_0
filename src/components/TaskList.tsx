3c0f4131c1cbe3493019a16946bb01390096d9c4
gustavoGGV <gustavo.vieira.tds2023@gmail.com>
2025-05-23 10:52:32 -0300

    feat: delete done tasks

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -5,6 +5,22 @@ import AddButton from "./AddButton";
 const TaskList = () => {
   const [tasks, setTasks] = useState(["Ir ao mercado"]);
 
+  const deleteTask = (taskToDelete: string) => {
+    // Had to return a React.MouseEvent because TypeScript says it's needed with onClick events.
+    return async (event: React.MouseEvent) => {
+      // Delay 1 second (1000 ms).
+      await setDelay(1000);
+      // Delete the marked task.
+      setTasks((tasks) => tasks.filter((task) => task !== taskToDelete));
+      event.preventDefault();
+    };
+  };
+
+  // Function to set delay.
+  function setDelay(delay: number) {
+    return new Promise((res) => setTimeout(res, delay));
+  }
+
   return (
     <div className="component p-3 rounded-4">
       <div className="d-flex">
@@ -15,7 +31,7 @@ const TaskList = () => {
 
       {tasks.map((task, key) => (
         <div className="d-flex" key={key}>
-          <input className="ms-2" type="checkbox" />
+          <input className="ms-2" type="checkbox" onClick={deleteTask(task)} />
           <h3 className="ms-2">{task}</h3>
         </div>
       ))}
