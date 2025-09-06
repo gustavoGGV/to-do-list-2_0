c19b01a64cb63e72af8bbb8d9426ee03d99918ad
gustavoGGV <gustavo.vieira.tds2023@gmail.com>
2025-09-06 13:44:24 -0300

    feat: add github page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,12 @@
 const NavBar = () => {
   return (
-    <div className="component p-3">
-      <h1>Do it!</h1>
+    <div className="component p-3 d-flex justify-content-between">
+      <h1 className="logo w-25">Do it!</h1>
+      <h1>
+        <a className="text-decoration-none text-white" href="https://github.com/gustavoGGV" target="_blank">
+          <i className="bi bi-github me-2"></i>gustavoGGV
+        </a>
+      </h1>
     </div>
   );
 };
